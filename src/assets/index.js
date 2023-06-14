60e7b054aaee028321c4ba09bb3251f13dffac77
jermiel <kpessou.kounouho@edu.univ-paris13.fr>
2023-06-14 15:43:22 +0200

    fichiers src

---
+++ src/assets/index.js
@@ -0,0 +1,62 @@
+import logo from "./logo.svg";
+import backend from "./backend.png";
+import creator from "./creator.png";
+import mobile from "./mobile.png";
+import web from "./web.png";
+import github from "./github.png";
+import menu from "./menu.svg";
+import close from "./close.svg";
+
+import css from "./tech/css.png";
+import docker from "./tech/docker.png";
+import langC from "./tech/langC.png";
+import git from "./tech/git.png";
+import html from "./tech/html.png";
+import javascript from "./tech/javascript.png";
+import postgresql from "./tech/postgresql.jpg";
+import nodejs from "./tech/nodejs.png";
+import reactjs from "./tech/reactjs.png";
+import python from "./tech/python.png";
+import tailwind from "./tech/tailwind.png";
+import typescript from "./tech/typescript.png";
+import threejs from "./tech/threejs.svg";
+
+import meta from "./company/meta.png";
+import zia from "./company/zia.jpg";
+import bliwe from "./company/bliwe.png";
+import diploop from "./company/diploop.png";
+
+import carrent from "./carrent.png";
+import jobit from "./jobit.png";
+import tripguide from "./tripguide.png";
+
+export {
+  logo,
+  backend,
+  creator,
+  mobile,
+  web,
+  github,
+  menu,
+  close,
+  css,
+  docker,
+  langC,
+  git,
+  html,
+  javascript,
+  postgresql,
+  nodejs,
+  reactjs,
+  python,
+  tailwind,
+  typescript,
+  threejs,
+  meta,
+  zia,
+  bliwe,
+  diploop,
+  carrent,
+  jobit,
+  tripguide,
+};

9f2071ae692250f8422b0c71b8bf2abe76c6313b
jermiel <kpessou.kounouho@edu.univ-paris13.fr>
2023-06-22 09:41:09 +0200

    about

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -30,8 +30,8 @@ const About = () => {
         <h2 className={styles.sectionHeadText}>Overview</h2>
       </motion.div>
       <motion.p variants={fadeIn("","",0.1,1)}>
-        I'm a skilled software developer with experience in TypeScript and
-        JavaScript, and expertise in frameworks like React, Node.js, and
+        I'm a skilled software developer with experience in
+        JavaScript, and expertise in frameworks like React and
         Three.js. I'm a quick learner and collaborate closely with clients to
         create efficient, scalable, and user-friendly solutions that solve
         real-world problems. Let's work together to bring your ideas to life!
